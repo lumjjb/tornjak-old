8e5a9e8b195a43944dad106dcf6022eba27cf60c
Mohammed Abdi <moabdi@Mohammeds-MBP.attlocal.net>
2021-04-19 14:23:54 -0400

    server select comp update

--- tornjak-frontend/src/components/select-server.component.js
+++ tornjak-frontend/src/components/select-server.component.js
@@ -173,14 +173,16 @@ class SelectServer extends Component {
 
         let managerServerSelector = (
             <div id="server-dropdown-div">
-                <label id="server-dropdown">Choose a Server:</label>
-                <select name="servers" id="servers" onChange={this.onServerSelect}>
-                    <optgroup label="Servers">
-                    <option value="none" selected disabled>Select an Option </option>
-                    <option value="none" selected disabled>{this.props.globalServerSelected} </option>
-                        {this.serverDropdownList()}
-                    </optgroup>
-                </select>
+                <label id="server-dropdown">Choose a Server</label>
+                <div className="servers-drp-dwn">
+                    <select name="servers" id="servers" onChange={this.onServerSelect}>
+                        <optgroup label="Servers">
+                        <option value="none" selected disabled>Select an Option </option>
+                        <option value="none" selected disabled>{this.props.globalServerSelected} </option>
+                            {this.serverDropdownList()}
+                        </optgroup>
+                    </select>
+                </div>
             </div>
         )
 
