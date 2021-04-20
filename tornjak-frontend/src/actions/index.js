a74a5a38a21d0946cfe27c9ef79c6437c346d383
Mohammed Abdi <moabdi@Mohammeds-MBP.attlocal.net>
2021-04-20 15:03:21 -0400

    add comments regarding redux actions

--- tornjak-frontend/src/actions/index.js
+++ tornjak-frontend/src/actions/index.js
@@ -1,73 +1,127 @@
-import { 
-    GLOBAL_SERVER_SELECTED, 
-    GLOBAL_ENTRIES_LIST, 
-    GLOBAL_AGENTS_LIST, 
+import {
+    GLOBAL_SERVER_SELECTED,
+    GLOBAL_ENTRIES_LIST,
+    GLOBAL_AGENTS_LIST,
     GLOBAL_SERVER_INFO,
     GLOBAL_TORNJAK_SERVER_INFO,
     GLOBAL_SERVERS_LIST,
     GLOBAL_SELECTOR_INFO,
- } from './types';
+} from './types';
 
-
+//Expected input - "ServerName"
+//Function - Sets the server selected in the redux state
 export function serverSelected(globalServerSelected) {
     return dispatch => {
         dispatch({
             type: GLOBAL_SERVER_SELECTED,
             payload: globalServerSelected
         });
-    }   
+    }
 }
 
+//Expected input - "TornjakServerInfo" [Saved as a large String]
+//Function - Sets the torjak server info of the selected server
 export function tornjakServerInfoUpdate(globalTornjakServerInfo) {
     return dispatch => {
         dispatch({
             type: GLOBAL_TORNJAK_SERVER_INFO,
             payload: globalTornjakServerInfo
         });
-    }   
+    }
 }
 
+//Expected input - 
+// {
+//     "data": 
+//     {
+//       "trustDomain": trustDomain,
+//       "nodeAttestorPlugin": nodeAttestorPlugin
+//     }
+// }
+//Function - Sets the server trust domain and nodeAttestorPlugin
 export function serverInfoUpdate(globalServerInfo) {
     return dispatch => {
         dispatch({
             type: GLOBAL_SERVER_INFO,
             payload: globalServerInfo
         });
-    }   
+    }
 }
 
+//Expected input - 
+// [
+//     "server1": 
+//     {
+//       "name": Server1Name,
+//       "address": Server1Address,
+//       "tls": false/true,
+//       "mtls": false/true,
+//     },
+//     "server2": 
+//     {
+//       "name": Server2Name,
+//       "address": Server2Address,
+//       "tls": false/true,
+//       "mtls": false/true,
+//     }
+// ]
+//Function - Sets the list of available servers and their basic info
 export function serversListUpdate(globalServersList) {
     return dispatch => {
         dispatch({
             type: GLOBAL_SERVERS_LIST,
             payload: globalServersList
         });
-    }   
+    }
 }
 
+//Expected input - 
+//[
+// "selector1": [
+//     {
+//       "label": "selector1:...."
+//     },
+//     {
+//       "label": "selector1:...."
+//     },
+//   ],
+//   "selector2": [
+//     {
+//       "label": "selector2:...."
+//     },
+//     {
+//       "label": "selector2:...."
+//     },
+//   ]
+//]
+//Function - Sets the list of available selectors and their options
 export function selectorInfo(globalSelectorInfo) {
     return dispatch => {
         dispatch({
             type: GLOBAL_SELECTOR_INFO,
             payload: globalSelectorInfo
         });
-    }   
+    }
 }
 
+//Expected input - List of entries with their info
+//Function - Sets/ updates the list of entries with their info
 export function entriesListUpdate(globalentriesList) {
     return dispatch => {
         dispatch({
             type: GLOBAL_ENTRIES_LIST,
             payload: globalentriesList
         });
-    }   
+    }
 }
 
+//Expected input - List of agents with their info
+//Function - Sets/ updates the list of agents with their info
 export function agentsListUpdate(globalagentsList) {
     return dispatch => {
         dispatch({
             type: GLOBAL_AGENTS_LIST,
             payload: globalagentsList
         });
-    }   
+    }
 }
