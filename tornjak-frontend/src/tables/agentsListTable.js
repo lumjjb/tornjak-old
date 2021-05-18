71c550e0cacc2b2d3e29e46a428c268460255973
Mohammed Abdi <mohammed.munir.abdi@ibm.com>
2021-05-18 11:25:22 -0400

    fix agent id security issue

--- tornjak-frontend/src/tables/agentsListTable.js
+++ tornjak-frontend/src/tables/agentsListTable.js
@@ -62,8 +62,9 @@ class DataTableRender extends React.Component {
             listtabledata[i]["spiffeid"] = "spiffe://" + listData[i].props.agent.id.trust_domain + listData[i].props.agent.id.path;
             listtabledata[i]["info"] = JSON.stringify(listData[i].props.agent, null, ' ');
             if (this.props.globalAgentsWorkLoadAttestorInfo !== undefined) {
-                var check_id = this.props.globalAgentsWorkLoadAttestorInfo.filter(agent => (parseInt(agent.id) === i + 1));
-                if (check_id.length !== 0) {
+                //var check_id = this.props.globalAgentsWorkLoadAttestorInfo.filter(agent => (parseInt(agent.id) === i + 1));
+                var check_id = this.props.globalAgentsWorkLoadAttestorInfo.filter(agent => (agent.spiffeid) === listtabledata[i].spiffeid);
+                if (check_id.length !== 0 && (check_id[0].spiffeid == listtabledata[i].spiffeid)) {
                     listtabledata[i]["plugin"] = check_id[0].plugin;
                 }
                 else {
