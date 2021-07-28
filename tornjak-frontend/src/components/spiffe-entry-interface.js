5164c4011f93ed96a9d0b0be551d9ba3c2a5f6ee
Mohammed Abdi <mohammed.munir.abdi@ibm.com>
2021-07-28 13:47:39 -0400

    nits in spiffe-entry-interface

--- tornjak-frontend/src/components/spiffe-entry-interface.js
+++ tornjak-frontend/src/components/spiffe-entry-interface.js
@@ -8,62 +8,56 @@ class SpiffeEntryInterface extends Component {
   getAgentExpiryMillisecondsFromEpoch(entry) {
     if (typeof entry !== 'undefined') {
       return entry.x509svid_expires_at * 1000
-    } else {
-      return ""
     }
+    return ""
   }
 
   getEntryExpiryMillisecondsFromEpoch(entry) {
     if (typeof entry !== 'undefined' && typeof entry.expires_at !== 'undefined') {
       return entry.expires_at * 1000
-    } else {
-      return ""
     }
+    return ""
   }
 
   getEntryAdminFlag(entry) {
-    if (typeof entry !== 'undefined' && typeof entry.admin !== 'undefined') {
-      return entry.admin
-    } else {
-      return false
-    }
+    return typeof entry !== 'undefined' &&
+           typeof entry.admin !== 'undefined' &&
+           entry.admin;
   }
 
   // check if format strictly adhered to
   checkSpiffeidValidity(trust_domain, path) {
     if (typeof trust_domain === 'undefined' || typeof path === 'undefined') {
       return false
+    } else if (trust_domain.length === 0 || path.length === 0) {
+        return false
     } else if (trust_domain.charAt(0) === '/') {
       return false
     } else if (trust_domain.slice(-1) === "/" && path.charAt(0) !== "/") {
       return true
-    } else {
-      return (trust_domain.slice(-1) !== "/" && path.charAt(0) === "/")
     }
+    return (trust_domain.slice(-1) !== "/" && path.charAt(0) === "/")
   }
 
   getAgentSpiffeid(entry) {
     if (typeof entry !== 'undefined' && this.checkSpiffeidValidity(entry.id.trust_domain, entry.id.path)) {
       return "spiffe://" + entry.id.trust_domain + entry.id.path
-    } else {
-      return ""
     }
+    return ""
   }
 
   getEntrySpiffeid(entry) {
     if (typeof entry !== 'undefined' && this.checkSpiffeidValidity(entry.spiffe_id.trust_domain, entry.spiffe_id.path)) {
       return "spiffe://" + entry.spiffe_id.trust_domain + entry.spiffe_id.path
-    } else {
-      return ""
     }
+    return ""
   }
 
   getEntryParentid(entry) {
     if (typeof entry !== 'undefined' && this.checkSpiffeidValidity(entry.parent_id.trust_domain, entry.parent_id.path)) {
       return "spiffe://" + entry.parent_id.trust_domain + entry.parent_id.path
-    } else {
-      return ""
     }
+    return ""
   }
 
   getAgentStatusString(entry) {
@@ -78,13 +72,11 @@ class SpiffeEntryInterface extends Component {
         status = "Attested"
       }
       return status
-    } else {
-      return ""
     }
+    return ""
   }
 
   getAgentMetadata(spiffeid, metadataList) {
-    console.log(metadataList)
     if (typeof metadataList !== 'undefined') {
       var metadata = metadataList.filter(agent => (agent.spiffeid === spiffeid));
       if (metadata.length !== 0) {
